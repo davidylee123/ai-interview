5fab8a96a2c55b493dabc5135723de788789a9f7
davidylee123 <121922170+davidylee123@users.noreply.github.com>
2024-06-08 12:19:26 -0400

    Webcam and Mic Enabled

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
@@ -1,16 +1,64 @@
-import { Image, Webcam } from "lucide-react";
-import React from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Image, Mic } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import Webcam from "react-webcam";
+import useSpeechToText from "react-hook-speech-to-text";
 
 function RecordAnswerSection() {
+  const [userAnswer, setUserAnswer] = useState("");
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false,
+  });
+
+  useEffect(() => {
+    results.map((result) => {
+      setUserAnswer((prevAns) => prevAns + result?.transcript);
+    });
+  }, [results]);
+
   return (
-    <div>
-      <Image
-        src={"/webcam.png"}
-        width={200}
-        height={200}
-        className="absolute"
-      />
-      <Webcam />
+    <div className="flex items-center justify-center flex-col">
+      <div className="flex flex-col mt-20 justify-center items-center bg-black rounded-lg p-5">
+        <Image
+          src={"/webcam.png"}
+          width={200}
+          height={200}
+          className="absolute"
+        />
+        <Webcam
+          mirrored={true}
+          style={{
+            height: 300,
+            width: "100%",
+            zIndex: 10,
+          }}
+        />
+      </div>
+      <Button
+        variant="outline"
+        className="my-10"
+        onClick={isRecording ? stopSpeechToText : startSpeechToText}
+      >
+        {isRecording ? (
+          <h2 className="text-red-600 flex gap-2">
+            <Mic />
+            Stop Recording
+          </h2>
+        ) : (
+          "Record Answer"
+        )}
+      </Button>
+
+      <Button onClick={() => console.log(userAnswer)}>Show User Answer</Button>
     </div>
   );
 }
